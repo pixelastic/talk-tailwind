174296e951ca58e3cf0d38ec6e574c8ac46c40b4
Pixelastic <tim@pixelastic.com>
2019-12-02 12:56:12 +0100

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-commonjs */
+// Firts, should be able to import the config from the root
+// Second, should be able to import the norska-css (or sov) config to extend it
+// Both norska-css and sov should export their tailwind config
+console.info("loaded");
+module.exports = {};
+
