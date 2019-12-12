190451e60a3d0dde4d89b64705ec208e260c9e8b
Pixelastic <tim@pixelastic.com>
2019-12-12 17:00:46 +0100

    Adding 80% of the slides

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ const spacing = {
   '8p': '8%',
 };
 
-const scale = [1, 1.25, 1.5, 1.875, 2.25, 3, 4, 8, 12];
+const scale = [1, 1.25, 1.5, 1.875, 2.25, 3, 4, 6, 8, 10, 12];
 const scaleDown = [0.75, 0.5, 0.25];
 const baseFontSize = 2;
 let fontSize = _.transform(
@@ -27,28 +27,6 @@ fontSize = _.transform(
   },
   fontSize
 );
-console.info(fontSize);
-// const fontSize = {
-//   1: '2.625rem',
-//   2: '2.625rem',
-//   3: '2.625rem',
-//   4: '2.625rem',
-//   // 0: '0rem',
-//   // '06': '0.25rem',
-//   // '07': '0.5rem',
-//   // '08': '0.75rem',
-//   // '09': '0.875rem',
-//   // 1: '1rem',
-//   // 2: '1.125rem',
-//   // 3: '1.25rem',
-//   // 4: '1.5rem',
-//   // 5: '1.875rem',
-//   // 6: '2.25rem',
-//   // 7: '3rem',
-//   // 8: '4rem',
-//   // 9: '8rem',
-//   // 10: '12rem',
-// }
 
 newConfig.theme = {
   ...config.theme,
